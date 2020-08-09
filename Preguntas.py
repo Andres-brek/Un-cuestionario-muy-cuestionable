f55dd776c1965227f69de804e0778fb39ae8cfe1
Andres <idarragazabala2013@gmail.com>
2020-08-09 16:45:48 -0500

    Aqui hago nuevas preguntas

--- Preguntas.py
+++ Preguntas.py
@@ -3,6 +3,7 @@ def Video_games(Nombre):
     print("ok",Nombre,"vamos con la primera pregunta")
     Respuestas = 0
 
+
     #pregunta 1
     prgunta = print("""Vamos con algo facíl, cómo se llama el protagonista de la saga The legend of zelda?
     1)Zelda
@@ -14,7 +15,6 @@ def Video_games(Nombre):
     Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
     
 
-
     #pregunta 2
     prgunta = print("""El personaje de Luigi aparece en el juego super mario 64?
     1)Sí
@@ -23,7 +23,6 @@ def Video_games(Nombre):
     pregunta = "2"
     Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
    
-
 
     #pregunta 3
     prgunta = print("""En qué juego Kratos asesina a Zeus?
@@ -36,7 +35,65 @@ def Video_games(Nombre):
     Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
    
 
+    #preguntas 4
+    prgunta = print("""Cual fue el juego mas famoso en el genero Battle royal?
+    1)Free fire
+    2)Call of duty war zone
+    3)Apex legends
+    4)Fortnite""")
+    Respuesta = input()
+    pregunta = "4"
+    Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
+   
+
+     #preguntas 5
+    prgunta = print("""En que año salio a la venta el atari 2600?
+    1)1980
+    2)1960
+    3)1977
+    4)1976""")
+    Respuesta = input()
+    pregunta = "3"
+    Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
+    return Respuestas
+
+
+def Historia(Nombre):
+    print("Escogiste el examen de historia ")
+    print("ok",Nombre,"vamos con la primera pregunta")
+    Respuestas = 0
+
+
+    #pregunta 1
+    prgunta = print("""fecha de inicio de la primera guerra mundial
+    1)1915
+    2)1920
+    3)1918
+    4)1914""")
+    Respuesta = input()
+    pregunta = "4"
+    Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
     
+
+    #pregunta 2
+    prgunta = print("""la primer
+    1)Sí
+    2)No""")
+    Respuesta = input()
+    pregunta = "2"
+    Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
+   
+
+    #pregunta 3
+    prgunta = print("""En qué juego Kratos asesina a Zeus?
+    1)God of war 1
+    2)God of war 2
+    3)God of war 3
+    4)God of war ascension""")
+    Respuesta = input()
+    pregunta = "3"
+    Respuestas = Probador_de_respuestas(Respuesta,pregunta,Respuestas)
+   
 
     #preguntas 4
     prgunta = print("""Cual fue el juego mas famoso en el genero Battle royal?
@@ -61,7 +118,7 @@ def Video_games(Nombre):
     return Respuestas
 
     
-# aqui se reciben las respuestas otorgadas por el usuario y de devuelve un "+1" si la respuesta es correcta
+# aqui se reciben las respuestas otorgadas por el usuario y de devuelve un "+1" si la respuesta es correcta, por el contrario se devuelve el mismo valor 
 def Probador_de_respuestas(Respuesta,pregunta,Respuestas):
     calificacion = Respuestas
     if pregunta == Respuesta:
